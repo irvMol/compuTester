fe4e87f3303b50d944c1d09f6003483bf7694c78
Irving Molina <dev.irvmol@gmail.com>
2025-06-07 18:03:45 -0600

    adding french capability

--- assets/js/localization.js
+++ assets/js/localization.js
@@ -1,23 +1,23 @@
 async function initI18n() {
-  await i18next.use(i18nextHttpBackend).init({ lng: "en" });
+  await i18next.use(i18nextHttpBackend).init({ lng: 'en' });
 
   jqueryI18next.init(i18next, $, { useOptionsAttr: true });
 }
 
 function translatePage() {
   $(document.documentElement)
-    .attr("lang", i18next.language)
-    .attr("dir", i18next.dir(i18next.language));
+    .attr('lang', i18next.language)
+    .attr('dir', i18next.dir(i18next.language));
 
-  $("body").localize();
+  $('body').localize();
 }
 
 function bindLocaleSwitcher() {
-  const $switcher = $("[data-i18n-switcher]");
+  const $switcher = $('[data-i18n-switcher]');
 
   $switcher.val(i18next.language);
 
-  $switcher.on("change", async function () {
+  $switcher.on('change', async function () {
     await i18next.changeLanguage($switcher.val());
     translatePage();
   });
@@ -29,25 +29,25 @@ function bindLocaleSwitcher() {
   bindLocaleSwitcher();
 })();
 async function initI18n() {
-  await i18next.use(i18nextHttpBackend).init({ lng: "en" });
+  await i18next.use(i18nextHttpBackend).init({ lng: 'en' });
 
   jqueryI18next.init(i18next, $, { useOptionsAttr: true });
 }
 
 function translatePage() {
   $(document.documentElement)
-    .attr("lang", i18next.language)
-    .attr("dir", i18next.dir(i18next.language));
+    .attr('lang', i18next.language)
+    .attr('dir', i18next.dir(i18next.language));
 
-  $("body").localize();
+  $('body').localize();
 }
 
 function bindLocaleSwitcher() {
-  const $switcher = $("[data-i18n-switcher]");
+  const $switcher = $('[data-i18n-switcher]');
 
   $switcher.val(i18next.language);
 
-  $switcher.on("change", async function () {
+  $switcher.on('change', async function () {
     await i18next.changeLanguage($switcher.val());
     translatePage();
   });
